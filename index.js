2def3286c4d8e3e4a08c1231e6bb565b266b58f7
Shivada <mehmeterensariot15@gmail.com>
2026-02-18 18:21:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,40 +3,55 @@ const WebSocket = require("ws");
 
 const BOT_NAME = "BnshivadaCore";
 const PREFIX = "!";
+const CHATROOM_ID = "64710147";
 
 const commands = new Map();
-const commandFiles = fs.readdirSync("./commands");
+const commandFiles = fs.readdirSync("./komutlar");
+
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    commands.set(command.name, command);
+  const command = require(`./komutlar/${file}`);
+  commands.set(command.name, command);
 }
 
-const ws = new WebSocket("wss://kick-chat-url");
+const ws = new WebSocket(
+  "wss://ws-us2.pusher.com/app/de504dc5763aeef9ff52?protocol=7&client=js&version=7.0.3&flash=false"
+);
 
 ws.on("open", () => {
-    console.log(`${BOT_NAME} başlatıldı.`);
+  console.log(`${BOT_NAME} chat’e bağlandı`);
+
+  ws.send(JSON.stringify({
+    event: "pusher:subscribe",
+    data: {
+      channel: `chatrooms.${CHATROOM_ID}.v2`
+    }
+  }));
 });
 
 ws.on("message", (data) => {
-    const msg = JSON.parse(data);
+  const msg = JSON.parse(data.toString());
 
-    if (!msg.content) return;
-    if (!msg.content.startsWith(PREFIX)) return;
+  if (msg.event === "App\\Events\\ChatMessageEvent") {
+    const message = JSON.parse(msg.data);
+    const text = message.content;
+    const username = message.sender.username;
+    const isMod = message.sender.is_moderator;
 
-    const args = msg.content.slice(PREFIX.length).split(" ");
+    console.log(`${username}: ${text}`);
+
+    if (!text.startsWith(PREFIX)) return;
+
+    const args = text.slice(PREFIX.length).split(" ");
     const commandName = args.shift().toLowerCase();
 
     const command = commands.get(commandName);
     if (!command) return;
 
-  
-    if (command.modOnly && !msg.isMod) {
-        ws.send(JSON.stringify({
-            type: "message",
-            content: `[${BOT_NAME}] Bunu Kullanmak İçin Yeterli Yetkin Bulunmuyor`
-        }));
-        return;
+    if (command.modOnly && !isMod) {
+      console.log(`[${BOT_NAME}] Bunu Kullanmaya Yetkin Yetmiyor!`);
+      return;
     }
 
-    command.execute(ws, msg, args, BOT_NAME);
+    command.execute(ws, message, args, BOT_NAME);
+  }
 });
